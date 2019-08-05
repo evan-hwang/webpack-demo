89999d0a493d7b9aef070ccbeccc44da75aa2d0d
evan.h <evan.h@kakaocorp.com>
2019-08-05 17:29:52 +0900

    npm install lodash to production level

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 function component() {
     const element = document.createElement('div');
 
+    // _에 대해서 lodash가 포함되어야할 암시적 종속성이 있다.
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     return element;
