2fb587737cb932d0807ba6453a8a2a78ee9d42a1
evan.h <evan.h@kakaocorp.com>
2019-08-05 20:07:28 +0900

    image load completed.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    // 존재하는 div 태그에 imgae 추가
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
